ee50c5dec6d622bb97fec593997d9cd54c54e1bb
copybarajenkins <maggiedjx@hotmail.com>
2019-05-15 23:36:05 -0700

    use express/mongo to fetch all menu items

--- src/app/_models/menuItem.ts
+++ src/app/_models/menuItem.ts
@@ -1,9 +1,12 @@
+import { ItemCategory } from "./itemCategory";
+
 export class MenuItem {
     itemID: number;
     itemName: string;
     price: number;
     description: string;
     url?: string;
+    itemCategory: ItemCategory;
 
     constructor(values: Object = {}) {
         Object.assign(this, values);
