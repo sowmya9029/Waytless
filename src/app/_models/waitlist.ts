de3416a106bd2f3c79c175a6514aec6e76fd84d6
uschohk@hotmail.com <uschohk@hotmail.com>
2019-05-24 17:01:44 -0700

    add waitlist model

---
+++ src/app/_models/waitlist.ts
@@ -0,0 +1,14 @@
+import { Time } from "@angular/common";
+
+export class Waitlist {
+    customerName: string;
+    restaurantID: number;
+    groupSize: number;
+    joinTime: Date;
+    quotedtime: Date;
+    email : string;
+    phone : string;
+    notified: boolean;
+    confirmed:boolean;
+}
+
