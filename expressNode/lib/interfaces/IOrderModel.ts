b99be479c980da0d8aed6090f3b0b773a371a4b6
Spoorthi Bhat <spoorthicbhat@gmail.com>
2019-05-27 11:11:05 -0700

    No significant change

--- expressNode/lib/interfaces/IOrderModel.ts
+++ expressNode/lib/interfaces/IOrderModel.ts
@@ -2,7 +2,7 @@ import Mongoose = require("mongoose");
 import {MenuItemModel} from '../models/MenuItemModel'
 
 interface IOrderModel extends Mongoose.Document {
-    menuItem: MenuItemModel,
+    menuItemId: Number,
     quantity: Number,
     orderTime: Date,
     customerId : Number,
--- expressNode/lib/routes/Routes.ts
+++ expressNode/lib/routes/Routes.ts
@@ -82,7 +82,7 @@ export class Routes {
                 this.menuitem.deleteMenuBaseOnRestaurantAndMenuId(res,{"restaurantID":restId,"itemID":itemID});
             })
 
-            //update
+            //update menu item
             app.route('/menuitems').patch((req: Request, res: Response) => {
                 
                 var menuitems_entry = {
