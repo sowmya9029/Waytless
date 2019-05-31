ee2d1c01cc68d937eec4baea47ff77c412901188
Spoorthi Bhat <spoorthicbhat@gmail.com>
2019-05-31 13:18:37 -0700

    Adding the waitlist test file + minor cleanup in other files

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -14,11 +14,8 @@ import {OrderCartComponent} from './components/order-cart/order-cart.component'
 import { manageWaitlistComponent } from './components/manageWaitlist/manageWaitlist.component';
 
 const routes: Routes = [
-  //{ path: 'create', component: CreateComponent },
-  //{ path: 'edit/:id', component: EditComponent },
     { path: 'register', component: RegistrationComponent },
     { path: 'menu/:id', component: MenuComponent },
-  //{ path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'home', component: HomeComponent },
     { path: 'waitlist-entry/:id', component: WaitlistEntryComponent},
      { path: 'order-cart/:id',component: OrderCartComponent}, 
