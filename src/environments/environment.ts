abec2bad26b499d6e2fb7da8ec2d5a88251fe1a1
sowmya <sowmya9029@gmail.com>
2019-06-03 13:37:01 -0700

    change server url in env variables from localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+ // apiUrl:'https://localhost:8080/'
+  apiUrl: 'https://waytlessserver.azurewebsites.net/'
 };
