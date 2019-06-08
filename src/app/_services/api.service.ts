768cc81019ce61e1507a50277386e9c09add79b6
Spoorthi Bhat <spoorthicbhat@gmail.com>
2019-06-08 12:12:21 -0700

    Adding the mistakenly deleted part

--- src/app/_services/api.service.ts
+++ src/app/_services/api.service.ts
@@ -18,7 +18,32 @@ export class ApiService {
 
   public authenticateLogin(){
     return this.http.get(API_URL + '/auth/google', {}).subscribe(response => {
+      console.log("Google auth");
       console.log(response.status);
       });
+  }
+
+  public makeOrders(orders: Order[]) {
+    orders.forEach(o => {
+      console.log('posting order!');
+      console.log(o);
+      let response = this.http.post(API_URL + '/orders', {
+        "menuitemId" : o.menuItemId,
+        "quantity" : o.quantity,
+        "orderTime": o.orderTime,
+        "customerId" : o.customerId,
+        "restaurantID" : o.restaurantID
+      });
+      response.subscribe(r => console.log(r));
+    });
+  }
+
+  public getAllOrders(restaurantID: number, customerID: number): Observable<Order[]> {
+    return this.http.get(API_URL + '/orders/' + restaurantID + '/' + customerID)
+      .pipe(map((response) => {
+        const items: Order[] = response.json();
+        console.log(items);
+        return items;
+      }));
   }
 }
