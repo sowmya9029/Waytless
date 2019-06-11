8da0285546e9cf8fff3ae10d1458e50d48d711f8
copybarajenkins <maggiedjx@hotmail.com>
2019-06-11 00:38:56 -0700

    show wait entry time from db

--- src/app/components/waitlist-entry/waitlist-entry.component.ts
+++ src/app/components/waitlist-entry/waitlist-entry.component.ts
@@ -44,6 +44,7 @@ export class WaitlistEntryComponent implements OnInit {
               this.phone = waitlistItems[i].phone;
               this.email = waitlistItems[i].email;
               this.groupSize = waitlistItems[i].groupSize;
+              this.bookingTime = waitlistItems[i].joinTime;
               break;
             }
           }
